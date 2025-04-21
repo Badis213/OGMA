e6f0532981de4e9ceaf321d56a9be94cc79f8b62
Badis <bazizbadis13@gmail.com>
2025-04-21 23:17:58 +0200

    t

--- app.py
+++ app.py
@@ -25,9 +25,10 @@ app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SUPABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To disable a feature that uses memory for tracking
 # SQLAlchemy Configuration for Connection Pooling
-app.config['SQLALCHEMY_POOL_SIZE'] = 20
-app.config['SQLALCHEMY_POOL_RECYCLE'] = 1800  # Number of seconds after which a connection is recycled
-app.config['SQLALCHEMY_POOL_TIMEOUT'] = 30  # in seconds
+# Increase pool size and set a higher timeout
+app.config['SQLALCHEMY_POOL_SIZE'] = 20  # Increase the pool size (default is 5)
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 60  # Increase the timeout (default is 10 seconds)
+app.config['SQLALCHEMY_MAX_OVERFLOW'] = 40  # Allow some connections beyond the pool size (default is 10)
 
 
 # Initialisation des extensions
